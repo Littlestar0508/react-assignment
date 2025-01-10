9d9bd10b6bffc0898a7cc5b90f187e8b6afd9b03
Littlestar0508 <rist9860@gmail.com>
2025-01-11 01:06:16 +0900

    ProfileButton 컴포넌트 backgroundImage 스타일 변경 확인

--- public/components/ProfileButton.ts
+++ public/components/ProfileButton.ts
@@ -1,7 +1,11 @@
 import React from "../lib/react.js";
 
 function ProfileButton() {
-  return React.createElement("button", { type: "button", className: "add-profile-btn" });
+  return React.createElement("button", {
+    type: "button",
+    className: "add-profile-btn",
+    style: { backgroundImage: `url('../src/10-1.webp')`, backgroundSize: "100px 100px" },
+  });
 }
 
 export default ProfileButton;
