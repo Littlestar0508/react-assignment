8d1f2815186bdc72605e9fb8b207cde4d2188ced
Littlestar0508 <rist9860@gmail.com>
2025-01-11 00:33:56 +0900

    React 연결 및 컴포넌트 분리 구성

---
+++ public/components/Wrapper.ts
@@ -0,0 +1,7 @@
+import React from "../lib/react.js";
+
+function Wrapper() {
+  return React.createElement(React.Fragment, {}, "Hello World");
+}
+
+export default Wrapper;
--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -1,1 +1,6 @@
-console.log("hello");
+import Wrapper from "../components/Wrapper";
+import ReactDOM from "../lib/react-dom/client.js";
+import React from "../lib/react.js";
+
+const ReactDOMRoot = ReactDOM.createRoot(document.getElementById("react"));
+ReactDOMRoot.render(React.createElement(Wrapper));
