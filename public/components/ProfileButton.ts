62f2de1506c7c598c3e06404d109fdea49ca81dd
Littlestar0508 <rist9860@gmail.com>
2025-01-11 02:21:52 +0900

    마우스 호버 시 수정이 가능함을 알리는 UI 설정

--- public/components/ProfileButton.ts
+++ public/components/ProfileButton.ts
@@ -1,14 +1,19 @@
 import React from "../lib/react.js";
+import ProfileEdit from "./ProfileEdit.js";
 
 function ProfileButton(props: { size: string; image: string; render: () => void }) {
-  return React.createElement("button", {
-    type: "button",
-    className: "add-profile-btn",
-    style: { backgroundImage: `${props.image}`, backgroundSize: props.size },
-    onClick: () => {
-      props.render();
+  return React.createElement(
+    "button",
+    {
+      type: "button",
+      className: "add-profile-btn",
+      style: { backgroundImage: `${props.image}`, backgroundSize: props.size },
+      onClick: () => {
+        props.render();
+      },
     },
-  });
+    React.createElement(ProfileEdit)
+  );
 }
 
 export default ProfileButton;
---
+++ public/components/ProfileEdit.ts
@@ -0,0 +1,7 @@
+import React from "../lib/react.js";
+
+function ProfileEdit() {
+  return React.createElement("img", { className: "profile-edit-img", src: "../src/icon.svg" });
+}
+
+export default ProfileEdit;
