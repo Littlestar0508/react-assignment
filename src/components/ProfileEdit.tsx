df2e28db42ab656667411b420f47f14fe9369b45
Littlestar0508 <rist9860@gmail.com>
2025-01-19 14:58:38 +0900

    이미지 경로 수정

--- src/components/ProfileEdit.tsx
+++ src/components/ProfileEdit.tsx
@@ -1,7 +1,11 @@
 import React from "../lib/react.js";
 
 function ProfileEdit() {
-  return React.createElement("img", { className: "profile-edit-icon", src: "../src/icon.svg", alt: "" });
+  return React.createElement("img", {
+    className: "profile-edit-icon",
+    src: "/src/icon.svg",
+    alt: "",
+  });
 }
 
 export default ProfileEdit;
