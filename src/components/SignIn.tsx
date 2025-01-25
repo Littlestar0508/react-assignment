4b24ee2766029348f2dbced3a767630f93e544f1
Littlestar0508 <rist9860@gmail.com>
2025-01-26 00:19:34 +0900

    UI 뼈대 구현(기능X , 버튼X)

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,15 @@
 import FormInput from './FormInput';
+import '../styles/components/SignUpIn.css';
 
 export default function SignIn() {
   return (
     <form>
-      <FormInput label="이름" placeholder="입력 요망" />
+      <FormInput label="이메일" placeholder="user@company.io" type="email" />
+      <FormInput
+        label="패스워드"
+        placeholder="숫자, 영문 조합 6자리 이상 입력"
+        type="password"
+      ></FormInput>
     </form>
   );
 }
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,7 +4,26 @@ import '../styles/components/SignUpIn.css';
 export default function SignUp() {
   return (
     <form id="sign-up-form">
-      <FormInput label="이메일" placeholder="입력 바람"></FormInput>
+      <FormInput
+        label="이름"
+        placeholder="2글자 이상 입력"
+        type="text"
+      ></FormInput>
+      <FormInput
+        label="이메일"
+        type="email"
+        placeholder="user@company.io"
+      ></FormInput>
+      <FormInput
+        label="패스워드"
+        placeholder="숫자, 영문 조합 6자리 이상 입력"
+        type="password"
+      ></FormInput>
+      <FormInput
+        label="패스워드 확인"
+        placeholder="입력한 패스워드 다시 입력"
+        type="password"
+      ></FormInput>
     </form>
   );
 }
