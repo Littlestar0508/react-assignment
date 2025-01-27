9b5c2c09e27205a8c88fa52722ec328887a44ebf
Littlestar0508 <rist9860@gmail.com>
2025-01-27 13:01:25 +0900

    로고 적용(임시 커밋)

--- src/components/Coupang/CoupangLogin.tsx
+++ src/components/Coupang/CoupangLogin.tsx
@@ -5,8 +5,13 @@ import CoupangButton from './CoupangButton';
 export default function CoupangLogin() {
   return (
     <form className="CoupangForm">
-      <CoupangButton></CoupangButton>
+      <img
+        src="src/CoupangLogo.png"
+        className="coupangLogo"
+        alt="쿠팡 홈으로"
+      />
       <CoupangInput></CoupangInput>
+      <CoupangButton></CoupangButton>
       <input type="text" placeholder="테스트" className="test" />
     </form>
   );
