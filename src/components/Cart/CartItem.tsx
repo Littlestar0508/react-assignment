f7a3e3024fa569925f79546553bb035fbd6a28fa
Littlestar0508 <rist9860@gmail.com>
2025-02-04 02:04:57 +0900

    ul과 li list에 대한 css 스타일링 지정
    - CSS Module 방식

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,11 +1,14 @@
 import IncDecComponent from './IncDecComponent';
+import module from './CartStyles.module.css';
 
 export default function CartItem() {
   return (
-    <li>
-      <h3>1A 우유 900mL</h3>
-      <p>1880원</p>
-      <img src="src/cart-item-1.png" alt="" />
+    <li className={module['li-item']}>
+      <img src="src/cart-item-1.png" alt="" className={module['cart-image']} />
+      <div className={module['item-text']}>
+        <h3 className={module['item-title']}>1A 우유 900mL</h3>
+        <p className={module['item-desc']}>1,880원</p>
+      </div>
       <IncDecComponent />
     </li>
   );
--- src/components/Cart/CartList.tsx
+++ src/components/Cart/CartList.tsx
@@ -1,9 +1,10 @@
 import CartItem from './CartItem';
+import module from './CartStyles.module.css';
 
 export default function CartList() {
   return (
-    <div>
-      <ul>
+    <div className={module.container}>
+      <ul className={module['ul-container']}>
         <CartItem />
         <CartItem />
         <CartItem />
