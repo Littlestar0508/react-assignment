21c793c84fdcedf2f7caabd8ca8afabe52b90787
Littlestar0508 <rist9860@gmail.com>
2025-02-12 22:09:21 +0900

    music-card 컴포넌트 스타일링 및 마크업

--- src/components/SearchUi/components/MusicCard.tsx
+++ src/components/SearchUi/components/MusicCard.tsx
@@ -1,3 +1,15 @@
+import module from '../styles/MusicCardStyle.module.css';
+
 export default function MusicCard() {
-  return <div>ㅎㅇ</div>;
+  return (
+    <section className={module['music-card-container']}>
+      <img
+        className={module['album-cover']}
+        src="albumCover/60s-cardin.webp"
+        alt=""
+      />
+      <h3 className={module['song-title']}>{"60's Cardin"}</h3>
+      <p className={module['artist-name']}>글렌체크</p>
+    </section>
+  );
 }
